09905fac5faec574e6fb208418ea1faab552b24d
mihail <sapegin2005@yandex.ru>
2024-05-30 00:49:34 +0500

    Слегка починил вёрстку фильтров

--- urfolio/main/filters.py
+++ urfolio/main/filters.py
@@ -1,12 +1,25 @@
 import django_filters
 from .models import Project
+from django import forms
 
 class ProjectFilter(django_filters.FilterSet):
+    category = django_filters.MultipleChoiceFilter(
+        choices = Project.Category.choices,
+        widget=forms.CheckboxSelectMultiple()
+    )
+    course_number = django_filters.MultipleChoiceFilter(
+        choices=Project.CourseNumber.choices,
+        widget=forms.CheckboxSelectMultiple()
+    )
+    year = django_filters.MultipleChoiceFilter(
+        choices=Project.Year.choices,
+        widget=forms.CheckboxSelectMultiple()
+    )
     class Meta:
         model = Project
         fields = {
             'author': ['exact'],
-            'category': ['exact'],
-            'course_number': ['exact'],
-            'year': ['exact'],
+            #'category': ['exact'],
+            #'course_number': ['exact'],
+            #'year': ['exact'],
         }
